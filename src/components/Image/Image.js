5c78c5210dcc028603f3f9a94da5ee54f90aa4cf
yoav shaked <yoavcad@gmail.com>
2018-08-13 11:57:02 +0300

    task1 - filter

---
+++ src/components/Image/Image.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
+import './Image.scss';
+
+class Image extends React.Component {
+  static propTypes = {
+    dto: PropTypes.object,
+    galleryWidth: PropTypes.number
+  };
+
+  constructor(props) {
+    super(props);
+    this.calcImageSize = this.calcImageSize.bind(this);
+    this.state = {
+      size: 200,
+      filterName: ''
+    };
+    this.cliceHendlerFilter = this.cliceHendlerFilter.bind(this);
+  }
+
+  calcImageSize() {
+    const {galleryWidth} = this.props;
+    const targetSize = 200;
+    const imagesPerRow = Math.round(galleryWidth / targetSize);
+    const size = (galleryWidth / imagesPerRow);
+    this.setState({
+      size
+    });
+  }
+
+
+
+  componentDidMount() {
+    this.calcImageSize();
+  }
+
+  urlFromDto(dto) {
+    return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
+  }
+
+  cliceHendlerFilter(){
+    const filterNames = ['filter-grace', 'filter-contrast', 'filter-darken', 'filter-invert', 'filter-sepia'];
+    const max = 5;
+    const filterIdx = Math.floor(Math.random() * max);
+    this.setState({filterName: filterNames[filterIdx]});
+  }
+
+  render() {
+
+    return (
+      <div
+        className={`image-root ${this.state.filterName}`}
+        style={{
+          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
+          width: this.state.size + 'px',
+          height: this.state.size + 'px'
+        }}
+        >
+        <div>
+          <FontAwesome className="image-icon" name="clone" title="clone" />
+          <FontAwesome className="image-icon" name="filter" title="filter" onClick={this.cliceHendlerFilter}/>
+          <FontAwesome className="image-icon" name="expand" title="expand"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Image;
